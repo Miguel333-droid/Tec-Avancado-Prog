9bdb9048d80ee21f74a2dc1246521e27769b1edd
Miguel333-droid <miguelmarianooo07@gmail.com>
2025-03-25 09:01:38 -0300

    25/03/25

--- Classes/src/Model/Alunos.java
+++ Classes/src/Model/Alunos.java
@@ -1,5 +1,63 @@
 package Model;
 
 public class Alunos {
+	
+	private String nome;
+	private int idade;
+	private double peso;
+	private boolean formando;
+	private char sexo;
+	
+	
+	public Alunos (String nome, int idade, double peso, char sexo) {
+		this.nome = nome;
+		this.idade = idade;
+		this.peso = peso;
+		this.sexo = sexo;
+		formando = false;
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public void setFormando(boolean formando) {
+		this.formando = formando; 
+	}
+	
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public double getPeso() {
+		return peso;
+	}
+	
+	public boolean getFormando() {
+		return formando;
+	}
+	
+	public char getSexo() {
+		return sexo;
+	}
+	
+	
 
 }
--- Classes/src/Model/Turma.java
+++ Classes/src/Model/Turma.java
@@ -1,5 +1,55 @@
 package Model;
 
 public class Turma {
+	private String nome;
+	private String curso;
+	private int qtdeAlunos;
+	private int serie;
+	
+	
+	
+	public Turma (String nome, String curso, int qtdeAlunos, int serie) {
+		this.nome = nome;
+		this.curso = curso;
+		this.qtdeAlunos = qtdeAlunos;
+		this.serie = serie;
+		
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	public void setQtdeAlunos(int qtdeAlunos) {
+		this.qtdeAlunos = qtdeAlunos;
+	}
+	
+	
+	public void setSerie(int Serie) {
+		this.serie = serie;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCurso() {
+		return curso;
+	}
+	
+	public int getQtdeAlunos() {
+		return qtdeAlunos;
+	}
+	
+	public int getSerie() {
+		return serie;
+	}
+	
+	
 
 }
