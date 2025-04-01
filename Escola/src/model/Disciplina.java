06002e09f822273f627d21292c0bcff75d92f6b6
Miguel333-droid <miguelmarianooo07@gmail.com>
2025-04-01 11:39:11 -0300

    1/04/25

--- Escola/src/model/Disciplina.java
+++ Escola/src/model/Disciplina.java
@@ -1,4 +1,4 @@
-package Model;
+package model;
 
 public class Disciplina {
 	private String nome;
@@ -6,48 +6,48 @@ public class Disciplina {
 	private int semestre;
 	private boolean ofertada;
 	
-	
-	
 	public Disciplina (String nome, String professor, int semestre, boolean ofertada) {
 		this.nome = nome;
 		this.professor = professor;
 		this.semestre = semestre;
 		this.ofertada = ofertada;
-		
-		
 	}
 	
 	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public void setProfessor(String professor) {
-		this.professor = professor;
-	}
-	
-	public void setSemestre(int semestre) {
-		this.semestre = semestre;
-	}
-	
-	
-	public void setOfertada(boolean ofertada) {
-		this.ofertada = ofertada;
+		this.nome=nome;
 	}
 	
 	public String getNome() {
 		return nome;
 	}
 	
+	public void setProfessor(String professor) {
+		this.professor=professor;
+	}
+	
 	public String getProfessor() {
 		return professor;
+	}
+	
+	public void setSemestre(int semestre) {
+		this.semestre=semestre;
 	}
 	
 	public int getSemestre() {
 		return semestre;
 	}
 	
+	public void setOfertada(boolean ofertada) {
+		this.ofertada=ofertada;
+	}
+	
 	public boolean getOfertada() {
 		return ofertada;
 	}
-
+	
+	@Override
+	public String toString() {
+		return "O nome da turma é " + nome + ", o professor é " + professor + " e esta no " + semestre + " semestre.";
+	}
+	
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Tec-Avancado-Prog
+# TecAvancadaProg
  
